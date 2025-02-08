cbd86af335b7062cb6a64bb18f525852c77e95d8
nahyoomi <nahyomi2000@gmail.com>
2025-02-08 12:18:32 +0000

    :lipstick:feat:add scroll to newProfileModal

--- components/modals/NewProfileModal.tsx
+++ components/modals/NewProfileModal.tsx
@@ -27,17 +27,19 @@ interface NewProfileModalProps {
   errorMessage: string;
 }
 
-  const stylesModal = {
-    position: "absolute" as "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
-    p: 4,
-  };
+const stylesModal = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  maxHeight: "80vh",
+  overflowY: "auto", 
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const NewProfileModal: React.FC<NewProfileModalProps> = ({
   open,
